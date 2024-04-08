743e95dc9f84daa67bdcd7bb95669b3489a669c9
Alberes Pereira <alberesbass@hotmail.com>
2024-04-08 13:00:58 -0300

    defini o algoritmo e logica do codigo javascript para que fique mais simples e norteado a criacao das funcoes

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+/*
+    - Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html 
+
+            Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele 
+
+            Passo 2 - identificar o clique no botão 
+
+            Passo 3 - adicionar a classe ativo nos projetos escondidos 
+
+    - Objetivo 2 - esconder o botão de mostrar mais 
+            Passo 1 - pegar o botão e esconder ele
+*/
