c1248b6d91f184bbea622fdea9d3d738ab6cd3ad
Alberes Pereira <alberesbass@hotmail.com>
2024-04-13 20:26:51 -0300

    foi adicionado no html um botao que tera a funcao de esconder os projetos e foi feito a estilizado do botao na folha de estilo e definido a funcionalidade da folha de js

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,37 @@
-const botaoMostrarProjetos = document.querySelector(".btn-mostrar-projetos");
-const projetosInativos = document.querySelectorAll(".projeto:not(.ativo)");
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+const botaoEsconderProjetos = document.querySelector('.btn-esconder-projetos');
 
-botaoMostrarProjetos.addEventListener("click", () => {
-  mostrarMaisProjetos();
-  esconderBotao();
+botaoEsconderProjetos.classList.add("remover");
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+    botaoEsconderProjetos.classList.remove("remover");
 });
+
+botaoEsconderProjetos.addEventListener('click', () => {
+    esconderProjetos();
+    mostrarBotao();
+    botaoEsconderProjetos.classList.add("remover");
+})
+
+function mostrarBotao() {
+    botaoMostrarProjetos.classList.remove("remover");
+}
+
+function esconderProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.remove('ativo');
+    });
+}
 
 function esconderBotao() {
     botaoMostrarProjetos.classList.add("remover");
 }
 
 function mostrarMaisProjetos() {
-    projetosInativos.forEach((projetoInativo) => {
-        projetoInativo.classList.add("ativo");
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
     });
 }
